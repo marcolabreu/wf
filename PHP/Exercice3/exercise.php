fe359bf5cbc73acbb142125df91e10714b14d941
Marco L Abreu <marcolabreu@windows.ubuntu>
2019-02-25 16:23:42 +0100

    Solve exo 3

--- PHP/Exercice3/exercise.php
+++ PHP/Exercice3/exercise.php
@@ -1,4 +1,49 @@
 <?php
 
-$input;
-$winner;
+/**
+ * Get winner
+ *
+ * Return the winner of a game containing multiple rounds
+ *
+ * @param array[] $input The input containing rounds
+ *
+ * @return string
+ */
+
+function getWinner(array $input): string
+{
+    $brotherA = 0;
+    $brotherB = 0;
+
+    foreach ($input as [$cardA, $cardB]) {
+        if ($cardA > $cardB) {
+            $brotherA++;
+        } else if ($cardA < $cardB) {
+            $brotherB++;
+        }
+    }
+    if ($brotherA > $brotherB) {
+        return 'A';
+    }
+    return 'B';
+}
+
+function marcoWinner(array $input): string
+{
+    $brotherA = 0;
+    $brotherB = 0;
+
+    foreach ($input as [$cardA, $cardB]) {
+        if ($cardA > $cardB) {
+            $brotherA++;
+        } else if ($cardA < $cardB) {
+            $brotherB++;
+        }
+    }
+
+        return $brotherA > $brotherB ? 'A' : 'B';
+
+}
+
+//$winner = getWinner($input);
+$winner = marcoWinner($input);
