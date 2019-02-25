a68170aad980beba372982b1db15d903dc5b601e
Marco L. Abreu <marcolabreu@royalmail.top>
2019-02-25 12:20:56 +0100

    Solve exo 2 and bonus

--- PHP/Exercice2/exercise.php
+++ PHP/Exercice2/exercise.php
@@ -2,4 +2,8 @@
 $password;
 $salt;
 
-$saltedPassword = '';
+$passLen = strlen($password);
+$ceilPosition = ceil($passLen/2);
+$floorPosition = floor($passLen/2) + $passLen%2;
+
+$saltedPassword = substr($password, 0, $floorPosition) . $salt . substr($password, $ceilPosition);
