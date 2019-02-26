8633ecb9965dd4406fb3902573034d837db775af
Marco L. Abreu <marcolabreu@royalmail.top>
2019-02-26 15:25:55 +0100

    Solve exo 5 type 1

--- PHP/Exercice5/src/DimensionalMath/Vector/AngleCalculation.php
+++ PHP/Exercice5/src/DimensionalMath/Vector/AngleCalculation.php
@@ -1,13 +1,17 @@
 <?php
+namespace DimensionalMath\Vector;
 
-function getVectorAngle($vectorA, $vectorB) {
-   
-    $pi=pi();
-   
+function vectorAngle($vectorA, $vectorB)
+{
+
+    $pi = pi();
+
     list($ax, $ay) = $vectorA;
     list($bx, $by) = $vectorB;
-    
-    $i=(acos((($ax*$ay)+($bx*$by))/(sqrt(($ax*$ax)+($bx*$bx))*sqrt(($ay*$ay)+($by*$by))))*360)/($pi*2);
-   
+
+    $i = (acos((($ax * $ay) + ($bx * $by)) / (sqrt(($ax * $ax) + ($bx * $bx)) * sqrt(($ay * $ay) + ($by * $by)))) * 360) / ($pi * 2);
+
     return $i;
 }
+
+
