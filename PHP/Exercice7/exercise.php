3f71f35f82ca684e89e764e8a85f78675e99be53
Marco L. Abreu <marcolabreu@royalmail.top>
2019-02-28 15:27:39 +0100

    Solve exo 7

--- PHP/Exercice7/exercise.php
+++ PHP/Exercice7/exercise.php
@@ -1,1 +1,26 @@
 <?php
+
+function divide(float $dividend, float $divisor) : float
+{
+    if ($divisor == 0){
+        throw new RuntimeException('Division by 0 is not allowed');
+    } else {
+        return $dividend/$divisor;
+    }
+
+}
+
+function arrayDivide(array $array, float $divisor) : array
+{
+    $result = [];
+    try {
+      foreach ($array as $number){
+          $result[] =  divide($number, $divisor);
+      }
+        return $result;
+    } catch (Exception $exception){
+        return $array;
+    }
+
+}
+
