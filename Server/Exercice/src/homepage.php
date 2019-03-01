6b4b42376523786a448ddefe5b10f2ec39c9b4cb
Marco L. Abreu <marcolabreu@royalmail.top>
2019-03-01 16:44:09 +0100

    Refactor TagBeSill exo 1

--- Server/Exercice/src/homepage.php
+++ Server/Exercice/src/homepage.php
@@ -1,29 +1,12 @@
 <?php include('header.html'); ?>
+<?php include('pdo.php'); ?>
 
 <body>
 <h1>TagBeSill</h1>
 
-
-<?php $host = '127.0.0.1';
-$db = 'TagBeSill';
-$user = 'debian-sys-maint';
-$pass = 'kTAfY4463eRP9Q4Z';
-$charset = 'utf8';
-
-$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
-$options = [
-    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    PDO::ATTR_EMULATE_PREPARES => false,
-];
-try {
-    $pdo = new PDO($dsn, $user, $pass, $options);
-} catch (\PDOException $e) {
-    throw new \PDOException($e->getMessage(), (int)$e->getCode());
-}
-
-$stmt = $pdo->query('SELECT * FROM article');
-while ($row = $stmt->fetch()) {
+<?php
+$articles = $pdo->query('SELECT * FROM article');
+while ($row = $articles->fetch()) {
     ?>
 
     <article>
