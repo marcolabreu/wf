6e6235f8369eaa9692c5ff08f4cc534938a4d99e
Marco L. Abreu <marcolabreu@royalmail.top>
2019-03-04 11:37:12 +0100

    Starts

---
+++ Server/Exercice/correction/_get-token.php
@@ -0,0 +1,16 @@
+<?php
+require dirname(__DIR__).'/vendor/autoload.php';
+use Ramsey\Uuid\Uuid;
+
+function generateToken() : string {
+    $cipher = "aes-256-gcm";
+    $iv = openssl_random_pseudo_bytes(openssl_cipher_iv_length($cipher));
+    $requestId = Uuid::uuid1();
+
+    return openssl_encrypt($requestId, $cipher, gethostname(), OPENSSL_ZERO_PADDING, $iv, $tag);
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+    $token = generateToken();
+}
+?>
